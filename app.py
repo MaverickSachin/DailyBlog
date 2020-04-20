bdafff372a67ca79bf4a47b9346d6c79b4aebf27
Sachin Ailwar <SachinAilwar23@gmail.com>
2020-04-20 12:32:00 +0530

    Added user sign up and login in feature

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from decouple import config
+from .forms import SignUpForm, LogInForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 posts = [
     {
@@ -27,6 +30,28 @@ def index():
 @app.route("/about/")
 def about():
     return render_template("about.html")
+
+
+@app.route("/signup/", methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('index'))
+    return render_template("signup.html", form=form)
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == "MaverickSachin@gmail.com" and form.password.data == "password123":
+            flash("You have been logged in!", "success")
+            return redirect(url_for('index'))
+        else:
+            flash("Login unsuccessful. Please check username and password.", "danger")
+
+    return render_template("login.html", form=form)
 
 
 if __name__ == "__main__":
