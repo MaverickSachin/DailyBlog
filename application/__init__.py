3199e0467f8e19b213fc44652354c478840a579d
Sachin Ailwar <SachinAilwar23@gmail.com>
2020-06-04 19:00:38 +0530

    Customized errors handlers are implemented

--- application/__init__.py
+++ application/__init__.py
@@ -20,7 +20,7 @@ mail = Mail()
 def create_app(config_class=Config):
     """ Construct the core application"""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object(config_class)
 
     db.init_app(app=app)
     bcrypt.init_app(app=app)
@@ -31,10 +31,12 @@ def create_app(config_class=Config):
         from application.users.views import users  # Import users blueprint
         from application.main.views import main  # Import main blueprint
         from application.posts.views import posts  # Import posts blueprint
+        from application.errors.handlers import errors  # Import errors blueprint
 
         app.register_blueprint(users)
         app.register_blueprint(main)
         app.register_blueprint(posts)
+        app.register_blueprint(errors)
 
         db.create_all()  # Create database tables for our data models
 
