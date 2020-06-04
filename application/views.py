310816016b3dbdd3981c2bf5352b30cae0fcc90c
Sachin Ailwar <SachinAilwar23@gmail.com>
2020-06-04 11:26:57 +0530

    Added Pagination functionality

--- application/views.py
+++ application/views.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
     return render_template("index.html", posts=posts)
 
 
@@ -149,3 +150,13 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been delete!", "success")
     return redirect(url_for("index"))
+
+
+@app.route("/user/<string:username>/")
+def user_posts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=2)
+    return render_template("user_posts.html", user=user, posts=posts)
